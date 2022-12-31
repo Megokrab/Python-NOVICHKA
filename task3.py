643c6596e12a58962a072fb351bc1ef41bf7c33b
Evgeniy <DarckSideWalker@gmail.com>
2022-12-31 16:17:00 +0600

    Izmeneniya v task3

--- task3.py
+++ task3.py
@@ -1,8 +1,9 @@
 name_a = input("Name")
 name_b = "Evgeniy"
+name_c = "Kiril"
 if name_a == name_b :
     print("Hello, admin!")
+elif name_a == name_c :
+    print("Moderator time ><")
 else:
     print("Hello, mere mortal!")
-
-
