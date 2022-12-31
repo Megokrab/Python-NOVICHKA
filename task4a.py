8ba77b2bfa39ba15e6c56f08d2f7dfa40e1fd6bd
Evgeniy <DarckSideWalker@gmail.com>
2022-12-31 16:51:16 +0600

    perviy variant

---
+++ task4a.py
@@ -0,0 +1,9 @@
+print("Напишите - виж некуцй")
+def reversed1(variable):
+    res=''
+    for i in range(len(variable)-1,-1,-1):
+        res+=variable[i]
+    return res
+
+n = reversed1(input())
+print(n)
